9aab02bbc1541ad1f78fca951b8c9c939cae2eb8
BZR4 <BZR4@users.noreply.github.com>
2026-03-11 21:25:36 -0300

    Initial Commit

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
+import java.util.Collections;
+
 import domain.Curso;
 import domain.Professor;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Professor p1 = new Professor();
+        Professor p1 = new Professor(Collections.emptyList());
         Curso dsm = new Curso();
-
+        dsm.nome = "Desenvolvimento de Software Multiplataforma";
         p1.ensinar(dsm);
     }
 }
--- src/domain/Professor.java
+++ src/domain/Professor.java
@@ -1,10 +1,24 @@
 package domain;
 
+import java.util.List;
+
 public class Professor {
+
+    private List<Aluno> alunos;
+
+    public Professor(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
 
     public void ensinar(Curso curso) {
         System.out.println("O professor está ensinando o curso: " 
         + curso.nome.toUpperCase());
+
+        for (Aluno aluno : alunos) {
+            for (String conhecimento : curso.getConhecimentos()) {
+                aluno.assimilarConhecimento(conhecimento);
+            }
+        }  
     }
 
 }
