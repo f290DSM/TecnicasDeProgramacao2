858f30fe2a93b8227e2e7790fd8780840c814da6
BZR4 <BZR4@users.noreply.github.com>
2026-03-11 21:50:45 -0300

    Inicio Herança

---
+++ src/domain/pokemons/GreNinja.java
@@ -0,0 +1,8 @@
+package domain.pokemons;
+
+public class GreNinja extends Pokemon {
+        
+    public void shuriken() {
+        System.out.println("GreNinja usou Shuriken!");
+    }
+}
---
+++ src/domain/pokemons/Pikachu.java
@@ -0,0 +1,11 @@
+package domain.pokemons;
+
+public class Pikachu {
+    public String nick;
+    public Integer cp;
+    public Integer hp;
+    
+    public void thunderbolt() {
+        System.out.println("Pikachu usou Thunderbolt!");
+    }
+}
