f90c77971e1cf950f0130d89b53029f843be52ef
BZR4 <BZR4@users.noreply.github.com>
2026-03-18 19:21:06 -0300

    Polimorfismo

--- src/domain/pokemons/Charmander.java
+++ src/domain/pokemons/Charmander.java
@@ -2,6 +2,10 @@ package domain.pokemons;
 
 public class Charmander extends Pokemon {
         
+    public Charmander(String nick, Integer cp, Integer hp) {
+        super(nick, cp, hp);
+    }
+
     public void ambar() {
         System.out.println("Charmander usou Ambar!");
     }
--- src/domain/pokemons/GreNinja.java
+++ src/domain/pokemons/GreNinja.java
@@ -1,7 +1,11 @@
 package domain.pokemons;
 
 public class GreNinja extends Pokemon {
-        
+
+    public GreNinja(String nick, Integer cp, Integer hp) {
+        super(nick, cp, hp);
+    }
+
     public void shuriken() {
         System.out.println("GreNinja usou Shuriken!");
     }
